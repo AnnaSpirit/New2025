573671dd9cfd3fda3428eb1b6b97563342603491
Anna Spirit <annalevy.training@gmail.com>
2025-03-21 11:13:49 +0200

    ...555.

---
+++ DI_Bootcamp_Stage1/04-Full-Stack Coding Bootcamp/1 Week - Python and Web Programming/5D - Dictionaries/testing_git.py
@@ -0,0 +1,4 @@
+ ! [rejected]        master -> master (non-fast-forward)
+error: failed to push some refs to 'https://github.com/AnnaSpirit/New2025.git'
+
+git commit -m "Ignorer le dossier .history dans z_Config"
