f5911a483dd761ce8140a34a042195d1e5ed4b9c
Anna Spirit <annalevy.training@gmail.com>
2025-03-24 22:29:40 +0200

    W2D2 Functions Exercice XP-Gold, Challenge Time, XP-Ninja ans Daily challenge NOT DONE YET. W2D3-4 Mini Project - Tic Tac Toe, Mini Project2 - Hangman DONE. Daily Challenge NOT DONE YET.

---
+++ DI_Bootcamp_Stage1/04-Full-Stack Coding Bootcamp/2 Week - Python and Web Programming and Intro to OOP/1+2D - Functions/5.Daily Challenge-Solve the Matrix.py
@@ -0,0 +1,25 @@
+EXERCICE: Daily challenge: Solve the Matrix --Given a “Matrix” string:
+
+# 7ii
+# Tsx
+# h%?
+# i #
+# sM 
+# $a 
+# #t%
+# ^r!
+
+# Matrix: A matrix is a two-dimensional array. It is a grid of numbers arranged in rows and columns.
+# To reproduce the grid, the matrix should be a 2D list, not a string
+
+
+# To decrypt the matrix, Neo reads each column from top to bottom, starting from the leftmost column, selecting only the alpha characters and connecting them. Then he replaces every group of symbols between two alpha characters by a space.
+
+# Using his technique, try to decode this matrix.
+
+# Hints:
+# Use
+# ● lists for storing data
+# ● Loops for going through the data
+# ● if/else statements to check the data
+# ● String for the output of the secret message
